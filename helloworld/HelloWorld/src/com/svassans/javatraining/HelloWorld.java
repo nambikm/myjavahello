abcc81abf5387c687d04841caa4d6d9bd39bd2ed
Nambi Madhi <nambikm@live.com>
2014-08-01 12:35:07 -0700

    Extended Helloworld

---
+++ helloworld/HelloWorld/src/com/svassans/javatraining/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.svassans.javatraining;
+
+
+public class HelloWorld {
+	public static void  main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World! Oh I see..");
+		return;
+	}
+}
