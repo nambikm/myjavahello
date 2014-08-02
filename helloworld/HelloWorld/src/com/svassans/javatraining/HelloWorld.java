f6934bceb18d705d4209d5fc42a96b0d254330aa
Nambi Madhi <nambikm@live.com>
2014-08-02 08:55:04 -0700

    all comit

--- helloworld/HelloWorld/src/com/svassans/javatraining/HelloWorld.java
+++ helloworld/HelloWorld/src/com/svassans/javatraining/HelloWorld.java
@@ -1,10 +1,51 @@
+
+
+
 package com.svassans.javatraining;
+
+import java.util.Date;
+
+
 
 
 public class HelloWorld {
+	static private String[] months = {
+		"Jaunuary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+	};
 	public static void  main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World! Oh I see..");
-		return;
+		System.out.println("Number of args: " + args.length);
+		
+		for (int i = 0; i < args.length; i++) {
+			
+			System.out.println(args[i]);			
+		}
+		
+		for (String month : months) {
+			System.out.println(month);			
+		}	
+		
+		loopme(5);
+		
+		double result = addMultipleDoubles(1,2,3,4,5);
+		
+		System.out.println("the value is " + result);			
+		
+		return;		
+
+	}
+	public static double addMultipleDoubles(double ... values) {
+		double result = 0.0;
+		for (double d : values) {
+			result+= d;
+		}
+		return result;
+	}
+	public static void loopme(int top) {
+		for (int i = 0; i < top ; i++)
+		{
+			System.out.println("loop #" + (i+1));
+		}
 	}
 }
